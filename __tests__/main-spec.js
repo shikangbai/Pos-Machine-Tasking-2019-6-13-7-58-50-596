298ea55606624ada47e5f3d56c014604c53f6c97
SKB <1215237417@qq.com>
2019-08-22 16:05:44 +0800

    add

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,32 @@
-const add = require('../main');
+const {countProducts,fetchProduct,generateReceiptItems,countTotalPrice,assemble} = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
+
+const countedCodes=countProducts(['0003','0003','0001']);
+const code='0001';
+const product=fetchProduct(code);
+var codes=generateReceiptItems(['0003','0003','0001'])
+console.log("generateReceiptItems:",codes);
+
+it('should printreceip',()=>{
+    var countToatolPriceInput=
+    [
+        {name:'Pepsi-Cola',price:5,count:2},
+        {name:'Coca Cola',price:3,count:1}
+    
+    ]
+    var toatolPrice = countTotalPrice(countToatolPriceInput);
+    console.log(toatolPrice);
+    })
+    
+    
+    it('打小票',()=>{
+    var assembleInput=
+    [
+        {name:'Pepsi-Cola',price:5,count:2},
+        {name:'Coca Cola',price:3,count:1}
+    ]
+    
+    var text = assemble(assembleInput,13);
+    console.log(text);
+    })
+
--- main.js
+++ main.js
@@ -1,5 +1,75 @@
-function add (left, right) {
-    return left + right;
+const db=[ {"id": "0001", "name" : "Coca Cola", "price": 3},
+{"id": "0002", "name" : "Diet Coke", "price": 4},
+{"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+{"id": "0004", "name" : "Mountain Dew", "price": 6},
+{"id": "0005", "name" : "Dr Pepper", "price": 7},
+{"id": "0006", "name" : "Sprite", "price": 8},
+{"id": "0007", "name" : "Diet Pepsi", "price": 9},
+{"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+{"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+{"id": "0010", "name" : "Fanta", "price": 12}
+]
+
+function countProducts(codes) {
+    var map=new Map();
+    for(let index=0;index<codes.length;index++){
+        var code=codes[index];
+        if(!map.has(code)){
+            map.set(code,{
+                code:code,
+                count:1
+            })
+        }else{
+            var item=map.get(code);
+            item.count++;
+            map.set(code,item);
+        }
+    }
+    var items=[];
+    map.forEach(function(item){
+        items.push(item);
+    })
+  return items
+}
+function fetchProduct(code){
+    for(let index=0;index<db.length;index++){
+        console.log(db[index])
+        if(db[index].id===code){
+            return{
+                price:db[index].price,
+                name:db[index].name
+            }
+        }
+}
+}
+function generateReceiptItems(codes){
+    var counterCodes=countProducts(codes);
+    var receipItems=[];
+    counterCodes.forEach(function(item){
+        var product=fetchProduct(item.code);
+        receipItems.push({
+            name:product.name,
+            price:product.price,
+            count:item.count
+        })
+    })
+    return receipItems;
+}
+function countTotalPrice(receipItems){
+    var total=0;
+    receipItems.forEach(function(item){
+        total=total+item.price*item.count;
+    });
+    return total;
 }
 
-module.exports = add;
+function assemble(assembleInput,num){
+    let str = "Receipts"+"\n"+"----------------"+"\n";
+    assembleInput.forEach(function(item){
+    str += item.name + "\t"+item.count+"\t"+item.price+"\n";
+    })
+    return str+"----------------"+"\n"+num;
+    }
+
+
+module.exports = {countProducts,fetchProduct,generateReceiptItems,countTotalPrice,assemble};
